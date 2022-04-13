d755e2f76b299badf23cfccddff1219c8ba21f6a
royjianxue <royjianxue@yahoo.com>
2022-04-12 22:55:58 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+namespace ConsoleGuessBook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool keepGoing = false;
+            Dictionary<string, int> guests = new Dictionary<string, int> ();
+            do
+            {
+                string names = Program.GetGuestName();
+                string numberOfGuests = Program.GuestsPerParty();
+
+                if(!int.TryParse(numberOfGuests, out int partyGuests))
+                {
+                    System.Console.WriteLine("Input error, numeric number expected, retrying...");
+                    Console.Write("Do you wish to continue: y to continue.. ANY to exit.");
+                    string answer = Console.ReadLine();
+                    if(answer.ToLower() == "y")
+                    {
+                        keepGoing = true;
+                    }
+                    else
+                    {
+                        keepGoing = false;
+                    }
+                }
+                else
+                {
+                    guests.Add(names, partyGuests);
+                }
+
+            } while (keepGoing);
+            
+            int total = 0;
+            foreach(KeyValuePair<string, int> guest in guests) //earch for keyValuePair
+            {
+                System.Console.WriteLine($"We have guest family {guest.Key}, and they have {guest.Value} members at the party");
+                total += guest.Value;
+            }
+        }
+
+        public static string GetGuestName()
+        {
+            System.Console.Write("Please record your name: ");
+            string lastName = Console.ReadLine();
+            return lastName;
+        }
+        public static string GuestsPerParty()
+        {      
+            System.Console.Write("How many guests are in your party: ");
+            string guests = Console.ReadLine();
+            return guests;
+        }
+
+
+            
+    }
+}
